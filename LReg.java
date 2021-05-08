7e2df6b582b5b1d828b2e47339ddbb043e99106e
bsravanthi9 <50476715+bsravanthi9@users.noreply.github.com>
2021-05-08 21:20:44 +0530

    Update LReg.java

--- LReg.java
+++ LReg.java
@@ -11,22 +11,22 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 public class LReg {
 	public static void main(String args[]) throws Exception {
-		DataSource src=new DataSource("C:\\Users\\dell\\eclipse-workspace\\org.dl\\src\\main\\java\\org\\dl\\clean_data.arff");
+		//DataSource src=new DataSource("C:\\Users\\dell\\eclipse-workspace\\org.dl\\src\\main\\java\\org\\dl\\clean_data.arff");
 		DataSource src1=new DataSource("C:\\Users\\dell\\eclipse-workspace\\org.dl\\src\\main\\java\\org\\dl\\test_data.arff");
 		
-		Instances ds=src.getDataSet();
-		ds.setClassIndex(ds.numAttributes()-1);
-		LinearRegression lr=new LinearRegression();
-		lr.buildClassifier(ds);
+		//Instances ds=src.getDataSet();
+		//ds.setClassIndex(ds.numAttributes()-1);
+		//LinearRegression lr=new LinearRegression();
+		//lr.buildClassifier(ds);
 		
 		
 		Instances ds1=src1.getDataSet();
 		ds1.setClassIndex(ds1.numAttributes()-1);
-		//LinearRegression lr1=new LinearRegression();
-		lr.buildClassifier(ds1);
+		LinearRegression lr1=new LinearRegression();
+		lr1.buildClassifier(ds1);
 		
 		Evaluation lr_eval=new Evaluation(ds1);
-		lr_eval.evaluateModel(lr, ds1);
+		lr_eval.evaluateModel(lr1, ds1);
 		System.out.println(lr_eval.toSummaryString());
 		 
 		
